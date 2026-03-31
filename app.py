aba3e27d7759a45e9f7d2209c372bd33841201f4
ffprotectemergency-ai <ffprotectemergency@gmail.com>
2026-03-31 09:55:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import json, os
+
+app = Flask(__name__)
+
+DB_FILE = "db.json"
+
+def load():
+    if not os.path.exists(DB_FILE):
+        return {}
+    return json.load(open(DB_FILE))
+
+def save(data):
+    json.dump(data, open(DB_FILE,"w"))
+
+db = load()
+
+@app.route("/")
+def home():
+    return "API RUNNING"
+
+@app.route("/user/<uid>")
+def get_user(uid):
+    return jsonify(db.get(uid, {}))
+
+@app.route("/update", methods=["POST"])
+def update():
+    data = request.json
+    uid = str(data["uid"])
+
+    if uid not in db:
+        db[uid] = {}
+
+    db[uid].update(data["data"])
+    save(db)
+
+    return {"ok": True}
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
